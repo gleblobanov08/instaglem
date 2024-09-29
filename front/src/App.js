db2f453fb9552b4e8705dfc1acbb7c1654c167ad
gleblobanov08 <135247482+gleblobanov08@users.noreply.github.com>
2024-09-29 08:38:11 +0000

    navbar started

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,8 @@
+import Pages from "./Components/Pages";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <Pages></Pages>
   );
 }
 
---
+++ front/src/Components/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Navbar from "./Navbar";
+
+const Home = () => {
+    return (
+        <div><Navbar></Navbar></div>
+    )
+}
+
+export default Home;
