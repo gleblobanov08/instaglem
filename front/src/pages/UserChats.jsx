137b3ec8fad47ff0a4e2c7bcf7779ffd08fc199b
gleblobanov08 <135247482+gleblobanov08@users.noreply.github.com>
2025-03-02 06:38:15 +0000

    v.2.2.7 - messenger updates

--- front/src/pages/UserChats.jsx
+++ front/src/pages/UserChats.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { collection, query, onSnapshot, orderBy, limit, getDocs, where } from 'firebase/firestore';
+import { collection, query, onSnapshot, getDocs, where, orderBy, limit } from 'firebase/firestore';
 import { db } from '../data/firebase';
 import { Link } from 'react-router-dom';
 import avatar from '../assets/avatar.png';
@@ -15,71 +15,57 @@ const UserChats = () => {
     if (!user) return;
 
     const chatsRef = collection(db, 'chats');
-    const q = query(chatsRef, orderBy("createdAt", "desc"));
+    const q = query(chatsRef, where('users', 'array-contains', user.uid));
 
     const unsubscribe = onSnapshot(q, async (querySnapshot) => {
       const fetchedChats = [];
+      const friendUIDs = [];
+
       querySnapshot.forEach((doc) => {
-        if (doc.data().users.includes(user.uid)) {
-          fetchedChats.push({ id: doc.id, ...doc.data() })
+        const chat = { id: doc.id, ...doc.data() };
+        fetchedChats.push(chat);
+        const friendUID = chat.users.find(uid => uid !== user.uid);
+        if (friendUID && !friendUIDs.includes(friendUID)) {
+          friendUIDs.push(friendUID);
         }
       });
-      const chatsWithFriendInfo = await Promise.all(
-        fetchedChats.map(async chat => {
-          const friendUID = chat.users.find(uid => uid !== user.uid); // Get the friend's UID
-          let friendData = { name: 'Unknown User', photoURL: avatar }; // Default values
 
-         if (friendUID) {
-          const q = query(collection(db, 'users'), where('uid', '==', friendUID));
-          const getDoc = await getDocs(q);
-          friendData = getDoc.docs[0].data();
-        }
+      const friendsQuery = query(collection(db, 'users'), where('uid', 'in', friendUIDs));
+      const friendsSnapshot = await getDocs(friendsQuery);
+      const friendsData = friendsSnapshot.docs.reduce((acc, doc) => {
+        acc[doc.data().uid] = doc.data();
+        return acc;
+      }, {});
+
+      const chatsWithLastMessages = await Promise.all(
+        fetchedChats.map(async (chat) => {
+          const friendUID = chat.users.find(uid => uid !== user.uid);
+          const friendData = friendsData[friendUID];
+
+          const messagesRef = collection(db, `/chats/${chat.id}/messages`);
+          const lastMessageQuery = query(messagesRef, orderBy('createdAt', 'desc'), limit(1));
+          const lastMessageSnapshot = await getDocs(lastMessageQuery);
+          const lastMessage = lastMessageSnapshot.docs[0]?.data()?.text || "No messages yet";
 
           return {
             ...chat,
             friendName: friendData.name,
-            friendAvatar: friendData.photoURL // Use friend's photoURL or default avatar
-          };
+            friendAvatar: friendData.photoURL,
+            lastMessage
+          }
         })
-      );
+      )
 
-      setChats(chatsWithFriendInfo);
+      setChats(chatsWithLastMessages);
     });
 
-    return () => unsubscribe()
-  }, [user])
-
-  const getLastMessage = async (chatId) => {
-    const messagesRef = collection(db, 'chats', chatId, 'messages');
-    const q = query(messagesRef, orderBy("createdAt", "desc"), limit(1)); // Assuming you have a `createdAt` field
-    const messagesSnapshot = await getDocs(q);
-    if (messagesSnapshot.docs.length > 0) {
-      if (messagesSnapshot.docs[0].data().uid === user.uid) {
-        return 'You: ' + messagesSnapshot.docs[0].data().text;
-      } else {
-        return messagesSnapshot.docs[0].data().text;
-      }
-    }
-      else {
-        return 'No messages yet'; // Assuming you have a `text` field
-      }
-  };
-
-  useEffect(() => {
-    const fetchLastMessages = async () => {
-      const messages = await Promise.all(chats.map(chat => getLastMessage(chat.id)));
-      setChats(prevChats => prevChats.map((chat, index) => ({ ...chat, lastMessage: messages[index] })));
-    };
-
-    if (chats.length > 0) {
-      fetchLastMessages();
-    }
-  }, [chats]);
+    return () => unsubscribe();
+  }, [user]);
 
   return (
     <div className='wrap'>
       <div className="fixed top-0 z-10 w-full bg-white">
-        <Navbar></Navbar>
+        <Navbar />
       </div>
       <div className="chat-lobby">
         <h2 className="text-2xl font-bold mt-12 mb-6">Your Chats</h2>
@@ -89,22 +75,22 @@ const UserChats = () => {
           <ul className="chat-list">
             {chats.map((chat) => (
               <Link to={`/chat/${chat.id}`} key={chat.id}>
-              <li className="chat-item">
-                <div className="avatar">
-                  <img src={chat.friendAvatar || avatar} alt="avatar"/>
-                </div>
-                <div className="chat-info">
-                  <h3 className="username">{chat.friendName}</h3>
-                  <p className='last-message'>{chat.lastMessage}</p>
-                </div>
-              </li>
+                <li className="chat-item">
+                  <div className="avatar">
+                    <img src={chat.friendAvatar || avatar} alt="avatar" />
+                  </div>
+                  <div className="chat-info">
+                    <h3 className="username">{chat.friendName}</h3>
+                    <p className='last-message'>{chat.lastMessage}</p>
+                  </div>
+                </li>
               </Link>
             ))}
           </ul>
         )}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default UserChats;
